a7c46f655037384e0b0b9cefcc5b89efc798301e
vietddude <imrbdnv@gmail.com>
2023-03-07 14:28:20 +0700

    add database object

--- app.js
+++ app.js
@@ -6,16 +6,26 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const morgan = require('morgan');
 
+dotenv.config();
+// Connect to database
+mongoose.connect(process.env.MONGODB_URI).then(
+  () => {console.log("Connected to database!");},
+  err => {
+    console.log(err);
+  }
+);
+
+
 const app = express();
+app.use(bodyParser.json({limit:"50mb"}));
+app.use(cors());
+app.use(morgan("common"));
 
+app.get("/api", (req, res) => {
+  res.status(200).json("Hello");
+})
 
-const router = express.Router();
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-
-app.listen('8000', () => {
+app.listen(8000, () => {
     console.log('Server is listening...');
 });
 
